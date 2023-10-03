44a2404302b140c04e4b379426ec928e23770369
ManishaPanu <113106578+ManishaPanu@users.noreply.github.com>
2023-10-04 00:03:30 +0530

    Update NumberofGoodPairs.cpp

--- NumberofGoodPairs.cpp
+++ NumberofGoodPairs.cpp
@@ -1,13 +1,19 @@
+//optimal O(n) time complexity
+
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> freq;
         int count = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(i<j && nums[i] == nums[j]) count++;
-            }
+        
+        for (int num : nums) {
+            freq[num]++;
         }
+        
+        for (auto &[_, f] : freq) {
+            count += f * (f - 1) / 2;
+        }
+        
         return count;
     }
 };
